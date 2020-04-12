ba19cf4b73b972267125c8538ddc892008017c6b
kele-12 <61654062+kele-12@users.noreply.github.com>
2020-04-12 11:19:04 +0800

    Add files via upload

---
+++ SQL06.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE OnlineShop
+GO
+USE OnlineShop
+GO
+CREATE TABLE Customer
+(
+int CHAR PRIMARY KEY,
+Card char(18) NOT NULL,
+Cname nVarChar(3) NOT NULL,
+RegDate dateTime NOT NULL,
+Meanvalue numeric(12,2)
+)
+CREATE TABLE Books
+(
+Bid CHAR(4)PRIMARY KEY,
+Bname  VARCHAR(20)NOT NULL,
+Author VARCHAR(15),
+Publisher VARCHAR(30),
+PublisherDate dateTime,
+Price money,
+Discount decimal(3,2)
+)
